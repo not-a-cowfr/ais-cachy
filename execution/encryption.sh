86e2a8350c97ee05c6dc97952d6377a312d7c458
= <=>
2025-12-30 16:22:30 +0100

    change of encryption to default options

--- execution/encryption.sh
+++ execution/encryption.sh
@@ -7,18 +7,9 @@ execute_encryption() {
         clear 
         echo "Wait For encryption to end"
 
-        [ -z "$hash" ] &&
-            hash="sha512"
-
-        [ -z "$keysize" ] &&
-            keysize="512"
-
-        [ -z "$itertime" ] &&
-            itertime="10000"
-
                     echo "$ENCRYPTION_PASSWORD1
                 $ENCRYPTION_PASSWORD2
-                " | cryptsetup luksFormat --type luks2 --batch-mode --verify-passphrase --hash "$hash" --key-size "$keysize" --iter-time "$itertime" --pbkdf argon2id --use-urandom "$ROOT" 
+                " | cryptsetup luksFormat "$ROOT" 
         
         echo "$ENCRYPTION_PASSWORD1" | cryptsetup luksOpen "$ROOT" artix 
 
