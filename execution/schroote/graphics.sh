71e41c265677b0ff99095a023dd066b7a1d2df48
user1312 <peroqq@proton.me>
2026-01-06 12:23:26 +0100

    some tweaks with nvidia drivers, mainly cleaning code

--- execution/schroote/graphics.sh
+++ execution/schroote/graphics.sh
@@ -5,52 +5,31 @@ execute_graphics() {
     for i in $GRAPHICS
     do
         case $i in
-        amd) install mesa xf86-video-amdgpu vulkan-radeon libva-mesa-driver ;;
-        
-        intel) install mesa xf86-video-intel vulkan-intel ;;
+            amd) install mesa xf86-video-amdgpu vulkan-radeon libva-mesa-driver ;;
+            intel) install mesa xf86-video-intel vulkan-intel ;;
+            nouveau) install mesa xf86-video-nouveau vulkan-nouveau  ;;
 
-        nouveau) install mesa xf86-video-nouveau vulkan-nouveau  ;;
+            nvidia-open) 
 
-        nvidia-open) 
-            case $KERNEL in 
-                linux) install nvidia-open nvidia-utils ;;
-                linux-lts) install nvidia-open-lts nvidia-utils ;;
-                linux-zen) install nvidia-open-dkms nvidia-utils ;;
-            esac
-            echo "modules_force_load: nvidia,nvidia_modeset,nvidia_uvm,nvidia_drm" >> /etc/booster.yaml
-            /usr/lib/booster/regenerate_images
-            NVIDIA_MODESET="nvidia-drm.modeset=1 nvidia-drm.fbdev=1"
-        ;;
+                if [ "$KERNEL" == "linux" ]
+                    then
+                        install nvidia-open nvidia-utils
+                    else
+                        install nvidia-open-dkms nvidia-utils
+                fi  ;;
 
-        nvidia-580) su $USERNAME << 'EOF'
-        source /mnt/schroote/variables.sh
-        install_aur nvidia-580xx-dkms nvidia-580xx-utils
-EOF
-        ;;
-
-        nvidia-470) su $USERNAME << 'EOF'
-        source /mnt/schroote/variables.sh
-        install_aur nvidia-470xx-dkms nvidia-470xx-utils
-EOF
-        ;;
-
-        nvidia-390) su $USERNAME << 'EOF'
-        source /mnt/schroote/variables.sh
-        install_aur nvidia-390xx-dkms nvidia-390xx-utils
-EOF
-         ;;
+            nvidia-580) install nvidia-580xx-dkms nvidia-580xx-utils ;;
+            nvidia-470) install nvidia-470xx-dkms nvidia-470xx-utils ;;
+            nvidia-390) install nvidia-390xx-dkms nvidia-390xx-utils ;;
         esac
     done
- 
-    if [[ "$GRAPHICS" =~ "nvidia" ]] && [[ "$GRAPHICS" != "nvidia-open" ]]
-    then
-        echo "modules_force_load: nvidia,nvidia_modeset,nvidia_uvm,nvidia_drm" >> /etc/booster.yaml
-        /usr/lib/booster/regenerate_images
-        NVIDIA_MODESET="nvidia-drm.modeset=1 nvidia-drm.fbdev=1"
-    fi
 
     if [[ "$GRAPHICS" =~ "nvidia" ]]
     then
+
+        echo "modules_force_load: nvidia,nvidia_modeset,nvidia_uvm,nvidia_drm" >> /etc/booster.yaml
+        /usr/lib/booster/regenerate_images
+        NVIDIA_MODESET="nvidia-drm.modeset=1 nvidia-drm.fbdev=1"
 
 echo '#!/bin/sh
 
@@ -62,7 +41,7 @@ case "$1" in
         /usr/bin/nvidia-sleep.sh "resume"
         ;;
 esac' > /lib64/elogind/system-sleep/nvidia
- 	SWAY_GPU="yes"
+    
     fi
 
         execute_modules
