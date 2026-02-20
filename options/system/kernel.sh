9604ff91e4893b711fb3a773a905c0d61a994191
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2026-02-19 19:42:10 -0800

    maybe cachyos kernel and repo support

--- options/system/kernel.sh
+++ options/system/kernel.sh
@@ -6,7 +6,7 @@ option_kernel() {
     --no-cancel \
     --extra-button --extra-label "MAP" \
     --menu  "Select your Linux Kernel:" 0 0 0 \
-    "linux" "" "linux-lts" "" "linux-zen" ""
+    "linux" "" "linux-lts" "" "linux-zen" "" "linux-cachyos" "" "linux-cachyos-lts" "" "linux-cachyos-bore" "" "linux-cachyos-rc" ""
     )
         [ "$?" == "3" ] && map
 
